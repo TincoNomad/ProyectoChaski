1462f504a0d5dd40dbc7ad88a25ea22b74169101
Renzo Tincopa <67519194+RenzoTinco@users.noreply.github.com>
2022-01-17 17:30:38 -0500

    La llave de Django cambia en cada computadora
    La borre por temas de seguridad

--- MensajeriaCore/settings.py
+++ MensajeriaCore/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1gsh6guits=%oav+=3_ix&6@a6$eqnk$d6=tbg-lgti^k%3%=2'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
