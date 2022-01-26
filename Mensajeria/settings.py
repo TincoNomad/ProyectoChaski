dad2d2fbf5ff790e1651073d887f9a351b93d120
Renzo Tincopa <rtincopa@gmail.com>
2022-01-26 12:39:02 -0500

    se agrego un comentario sobre Channels Layer en setting.py

--- Mensajeria/settings.py
+++ Mensajeria/settings.py
@@ -22,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = '1111111111111111111111111111111111111111111111111111111111111111111'
 
+             
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -134,9 +135,9 @@ LOGOUT_REDIRECT_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Configuracion de Channels Layer para trabajar de forma local
 CHANNEL_LAYERS = { 'default': {'BACKEND': 'channels.layers.InMemoryChannelLayer'}}
 
 
